b19a7f5db2d20c07d6a7e124f36a853047d7339c
miguel nog <mglsndc@gmail.com>
2021-03-14 15:52:32 -0300

    feat: add Title component

---
+++ src/components/Title/styles.ts
@@ -0,0 +1,13 @@
+import styled from 'styled-components/native';
+import colors from '../../styles/colors';
+
+export const Container = styled.Text`
+  color: ${colors.gray};
+  background: ${colors.primary};
+
+  text-transform: uppercase;
+  font-family: roboto_700;
+  font-size: 14px;
+
+  padding: 9px 0;
+`;
