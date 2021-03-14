6900ad4b5074c577dff2bb398ae5659599598cab
miguel nog <mglsndc@gmail.com>
2021-03-14 15:50:43 -0300

    feat: add ChannelList component

---
+++ src/components/ChannelList/index.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+
+import {
+  ChannelContainer,
+  LeftSide,
+  Avatar,
+  Column,
+  Username,
+  Info,
+  RightSide,
+  WhiteCircle,
+  List,
+} from './styles';
+
+const ChannelList: React.FC = () => {
+  const ChannelItem: React.FC = () => {
+    return (
+      <ChannelContainer>
+        <LeftSide>
+          <Avatar />
+          <Column>
+            <Username>@miguelsndc</Username>
+            <Info>36 new videos</Info>
+          </Column>
+        </LeftSide>
+        <RightSide>
+          <WhiteCircle />
+        </RightSide>
+      </ChannelContainer>
+    );
+  };
+
+  return (
+    <List>
+      <ChannelItem />
+      <ChannelItem />
+      <ChannelItem />
+      <ChannelItem />
+      <ChannelItem />
+      <ChannelItem />
+    </List>
+  );
+};
+
+export default ChannelList;
